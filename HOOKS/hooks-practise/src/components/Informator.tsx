4d965756f57e17ecd8ab39deaea840ffe63c122a
FBudn <111660524+FBudn@users.noreply.github.com>
2023-05-27 15:07:45 +0200

    added icons to informator

--- HOOKS/hooks-practise/src/components/Informator.tsx
+++ HOOKS/hooks-practise/src/components/Informator.tsx
@@ -1,12 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClipboardQuestion } from "@fortawesome/free-solid-svg-icons";
+import {
+  faClipboardQuestion,
+  faLink,
+  faCircleInfo,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Informator = () => {
   return (
     <div>
       <FontAwesomeIcon icon={faClipboardQuestion} />
+      <FontAwesomeIcon icon={faLink} />
+      <FontAwesomeIcon icon={faCircleInfo} />
     </div>
   );
 };
