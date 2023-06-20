c0a480105d1bd4d3bd194cb9c924896712f16f8f
FBudn <111660524+FBudn@users.noreply.github.com>
2023-06-20 15:16:56 +0200

    country onclick with axios, works but not finished

--- HOOKS/hooks-practise/src/components/ApiComponent.tsx
+++ HOOKS/hooks-practise/src/components/ApiComponent.tsx
@@ -1,24 +1,46 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import { TextInfo } from "../styles/styles";
+import { ButtonStyled, Input, TextInfo } from "../styles/styles";
 
 const ApiComponent = () => {
   const queryClient = useQueryClient();
+  const [country, setCountry] = useState("");
+  const handleInputCountry = (e: any) => {
+    setCountry(e.target.value);
+  };
+  const [capital, setCapital] = useState("");
+  const handleInputCapital = (e: any) => {
+    setCapital(e.target.value);
+  };
 
+  const COUNTRY: any = [];
+
+  const urlCountry = `https://restcountries.com/v3.1/name/poland?fullText=true`;
+  const urlCapital = `https://restcountries.com/v3.1/capital/${capital}`;
   const url = `https://restcountries.com/v3.1/all?fields=name,name`;
 
-  const countriesQuery = useQuery({
+  const handleCountry = async () => {
+    try {
+      await axios.get(urlCountry).then((response) => {
+        console.log(response.data[0].name.common);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  /* const countriesQuery = useQuery({
     queryKey: ["countries"],
     queryFn: async () => {
       await axios.get(url).then((response) => {
-        console.log(response.data);
+        return response.data;
       });
     },
   });
 
-  /* dodatkowe opcje, sprawdzanie różnych stanów
+   dodatkowe opcje, sprawdzanie różnych stanów
   if (postsQuery.isLoading) return <h1> Loading... </h1>;
   if (newPostsMutation.isLoading) return <h1> Loading... </h1>;
   if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
@@ -27,7 +49,20 @@ const ApiComponent = () => {
     <>
       <TextInfo>API component</TextInfo>
       <TextInfo>Country: </TextInfo>
+      <Input
+        type="text"
+        onChange={handleInputCountry}
+        placeholder="Write country name ex. Poland"
+      />
+      <ButtonStyled>Find capital</ButtonStyled>
       <TextInfo>Capital city: </TextInfo>
+      <Input
+        type="text"
+        onChange={handleInputCapital}
+        placeholder="Write capital name ex. Warsaw"
+      />
+      <ButtonStyled onClick={handleCountry}>Find country</ButtonStyled>
+      <TextInfo>C: {COUNTRY[0]}</TextInfo>
     </>
   );
 };
