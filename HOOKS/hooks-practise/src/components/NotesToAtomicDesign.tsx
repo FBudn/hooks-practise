9943a780909fd5260668614f4614436891e543ff
FBudn <111660524+FBudn@users.noreply.github.com>
2023-06-22 21:21:11 +0200

    ToDo list in NotesToAtomicDesign

--- HOOKS/hooks-practise/src/components/NotesToAtomicDesign.tsx
+++ HOOKS/hooks-practise/src/components/NotesToAtomicDesign.tsx
@@ -4,4 +4,12 @@ Molecules: Button-showSomething-Button, Button-ShowSomething
 Organisms: Every component with different purpose
 Templates: Everything connected, styled and with neccesary logic
 Pages: Buissnes logic + context and other wrappers 
+
+Other: 
+- Logic: reducer/redux or context + query over states in 4ex. API etc.
+- Names: to check
+- Styles: to improve someday
+- Informator: working
+- Add something with data and getting objects from objects, arrays etc. 
+ 
 */
