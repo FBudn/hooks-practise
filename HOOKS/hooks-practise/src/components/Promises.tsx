d6c1c1fd26501d8888ad3ab9bb9bf07668184500
FBudn <111660524+FBudn@users.noreply.github.com>
2023-06-22 21:05:14 +0200

    Promises methods component idea

---
+++ HOOKS/hooks-practise/src/components/Promises.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { TextInfo, Tilediv } from "../styles/styles";
+
+const Promises = () => {
+  return (
+    <>
+      <Tilediv> Promises </Tilediv>
+      <TextInfo>Win the race: </TextInfo>
+      <TextInfo>All Pokemons: </TextInfo>
+      <TextInfo>All settled Pokemons: </TextInfo>
+      <TextInfo>Any Pokemon: </TextInfo>
+      <TextInfo>Pokemons in loop: </TextInfo>
+      <TextInfo>Fetched Pokemons: </TextInfo>
+    </>
+  );
+};
+
+export default Promises;
