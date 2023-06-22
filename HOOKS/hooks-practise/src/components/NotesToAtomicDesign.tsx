99b7210c0f552ea44437da4f76f755c2d6e71121
FBudn <111660524+FBudn@users.noreply.github.com>
2023-06-22 21:16:51 +0200

    Atomic Design plan in component

---
+++ HOOKS/hooks-practise/src/components/NotesToAtomicDesign.tsx
@@ -0,0 +1,7 @@
+/*
+Atoms: All good + RowContainer as a Templates style & <MainAppContainer> too
+Molecules: Button-showSomething-Button, Button-ShowSomething
+Organisms: Every component with different purpose
+Templates: Everything connected, styled and with neccesary logic
+Pages: Buissnes logic + context and other wrappers 
+*/
