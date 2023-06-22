d22026344cc9a9a82d7f780470f9ebc5841abcad
FBudn <111660524+FBudn@users.noreply.github.com>
2023-06-22 21:07:13 +0200

    Destructuring component idea

---
+++ HOOKS/hooks-practise/src/components/Destructuring.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TextInfo, Tilediv } from "../styles/styles";
+
+const Destructuring = () => {
+  return (
+    <>
+      <Tilediv> Destructuring </Tilediv>
+      <TextInfo> Objects: </TextInfo>
+      <TextInfo> Arrays: </TextInfo>
+    </>
+  );
+};
+
+export default Destructuring;
